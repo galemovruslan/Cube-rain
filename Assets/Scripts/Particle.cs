bbb66a0072d8d4e8f64ab887d718329519c9d996
galemovruslan <82596071+galemovruslan@users.noreply.github.com>
2025-11-29 18:39:18 +0700

    Complete

---
+++ Assets/Scripts/Particle.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class Particle : MonoBehaviour
+{
+    [SerializeField] private Color _startColor;
+    [SerializeField] private Color _onTouchColor;
+    [SerializeField] private float _minLifeTime = 2f;
+    [SerializeField] private float _maxLifeTime = 5f;
+
+    private ObjectPool<Particle> _pool;
+    private MeshRenderer _renderer;
+    private bool _wasTouched = false;
+
+    private void Awake()
+    {
+        _renderer = GetComponent<MeshRenderer>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (_wasTouched) return;
+
+        if (collision.gameObject.TryGetComponent(out Platform _))
+        {
+            Touch();
+        }
+    }
+
+    public void SetPosition(Vector3 position)
+    {
+        transform.position = position;
+    }
+
+    public void Initialize(ObjectPool<Particle> pool)
+    {
+        _pool = pool;
+    }
+
+    public void ResetState()
+    {
+        _renderer.material.color = _startColor;
+        _wasTouched = false;
+    }
+
+    private void Touch()
+    {
+        _renderer.material.color = _onTouchColor;
+        _wasTouched = true;
+
+        StartCoroutine(nameof(WaitDestroy));
+    }
+
+    private IEnumerator WaitDestroy()
+    {
+        float seconds = UnityEngine.Random.Range(_minLifeTime, _maxLifeTime);
+        yield return new WaitForSeconds(seconds);
+        _pool.Release(this);
+    }
+}
---
+++ Assets/Scripts/RainSpawner.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class RainSpawner : MonoBehaviour
+{
+    [SerializeField] private Particle _prefab;
+    [SerializeField] private BoxCollider _spawnVolume;
+    [SerializeField] int _spawnSpeed = 5;
+
+    private ObjectPool<Particle> _pool;
+    private float _spawnOffset = 0f;
+
+    private void Awake()
+    {
+        _pool = new ObjectPool<Particle>(
+            createFunc: OnSpawn,
+            actionOnGet: OnGet,
+            actionOnRelease: OnRelease);
+    }
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(Spawn), _spawnOffset, 1.0f / _spawnSpeed);
+    }
+
+    private void Spawn()
+    {
+        _pool.Get();
+    }
+
+    private void OnRelease(Particle particle)
+    {
+        particle.gameObject.SetActive(false);
+    }
+
+    private void OnGet(Particle particle)
+    {
+        particle.SetPosition(GetPosition());
+        particle.ResetState();
+        particle.gameObject.SetActive(true);
+    }
+
+    private Particle OnSpawn()
+    {
+        Particle particle = Instantiate(_prefab);
+        particle.Initialize(_pool);
+        return particle;
+    }
+
+    private Vector3 GetPosition()
+    {
+        float xPosition = _spawnVolume.transform.position.x + (UnityEngine.Random.value * 2 - 1) * _spawnVolume.size.x / 2;
+        float yPosition = _spawnVolume.transform.position.y + (UnityEngine.Random.value * 2 - 1) * _spawnVolume.size.y / 2;
+        float zPosition = _spawnVolume.transform.position.z + (UnityEngine.Random.value * 2 - 1) * _spawnVolume.size.z / 2;
+
+        return new Vector3(xPosition, yPosition, zPosition);
+    }
+}
